c3e854c8a9d0f91dd8596ea30e60775365225ab0
xavierremigiocordovagarzon <xaviercordova2001@outlook.com>
2024-07-02 13:14:53 -0500

    Update2

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React Xavier Cordova</h1>
+        <h1>Hello React Xavier Cordova React</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
